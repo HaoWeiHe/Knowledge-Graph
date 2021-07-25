ac41012eea36bd297a3e0ed257305c200e1e76a9
HaoWei <haowei@HaoWei.local>
2021-07-24 22:28:35 -0700

    +vis

--- knowledgeGraph/__init__.py
+++ knowledgeGraph/__init__.py
@@ -2,7 +2,7 @@ import spacy
 model = "en_core_web_sm"
 nlp = spacy.load(model)
 from knowledgeGraph.information import InformationExtraction
-from knowledgeGraph.kg_vislization import Visualize
+from knowledgeGraph.visualization import Visualize
 infor_Extract = InformationExtraction()
 get_entity = infor_Extract.get_entities
 get_relation = infor_Extract.get_relations
